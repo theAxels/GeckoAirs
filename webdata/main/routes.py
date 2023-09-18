6594cb83a5f7bbefc8e08d2591aeb8bf15291563
theAxels <angkianto.laurence@binus.ac.id>
2023-09-18 11:29:04 +0700

    update result page

--- webdata/main/routes.py
+++ webdata/main/routes.py
@@ -59,10 +59,68 @@ def index():
             seattypeID = request.form.get('seattype')
             
             # print(fromCityID, toCityID, departing, seattypeID);
+            redirect(url_for('main.search_result'))
             
     seattypes = SeatType.query.all()
     cities = City.query.all()
     return render_template('index.html', cities=cities, seattypes=seattypes)
+
+@main.route('/result', methods=['GET', 'POST'])
+def search_result():
+    if request.method == 'POST':
+        action = request.args.get('action')
+        if action == 'login':
+            email = request.form.get('email1')
+            # print(email)
+            user = User.query.filter_by(email=email).first()
+            # print(user)
+            if user :
+                password = request.form.get('password1')
+                check = bcrypt.check_password_hash(user.password, password)
+                
+                if check == True:
+                    login_user(user)
+                    flash("User has been logged in succesfully!", 'success')
+                    return redirect(url_for('main.index'))
+                flash("Login Failed. Incorrect  password!", 'danger')
+                return redirect(url_for('main.index'))
+            flash("Login Failed. Unfound  email!", 'danger')
+            return redirect(url_for('main.index'))
+        if action == 'register':
+            dob = request.form.get('dob')
+            name = request.form.get('name')
+            phone_number = request.form.get('phone_number')
+            gender = request.form.get('gender')
+            email = request.form.get('email2')
+            password = request.form.get('password2')
+            
+            check = User.query.filter_by(email = email).first()
+            
+            if check : 
+                flash('Email for registration is not valid or already used', 'danger')
+                redirect(url_for('main.index'))
+            
+            hashed = bcrypt.generate_password_hash(password).decode('UTF-8')
+            user = User(dob = dob,name = name, phone_number = phone_number, gender = gender, email = email, password = hashed)
+            db.session.add(user)
+            db.session.commit()
+            flash('Accounts created successfully! please login ', 'success')
+            redirect(url_for('main.index'))
+        if action == 'search':
+            fromCityID = request.form.get('fromCity') 
+            toCityID = request.form.get('toCity')
+            departing = request.form.get('departing')
+            seattypeID = request.form.get('seattype')
+            
+            # print(fromCityID, toCityID, departing, seattypeID);
+            redirect(url_for('main.search_result'))
+            
+    seattypes = SeatType.query.all()
+    cities = City.query.all()
+    # return render_template('index.html', cities=cities, seattypes=seattypes)
+        
+        
+    return render_template('search_result.html', cities=cities, seattypes=seattypes)    
 
 @main.route('/logout')
 @login_required
