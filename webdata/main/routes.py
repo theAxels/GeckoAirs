03149a8926d119017abc79c5f96dcb914f66ee63
theAxels <angkianto.laurence@binus.ac.id>
2023-09-18 22:24:10 +0700

    update result and added book

--- webdata/main/routes.py
+++ webdata/main/routes.py
@@ -6,8 +6,9 @@ from flask_login import login_required, login_user, logout_user
 
 
 from webdata import bcrypt, db
-from webdata.models import User, City, City, SeatType, Flight, Route, Airlines, datetime
+from webdata.models import User, City, City, SeatType, Flight, Route, Airlines, datetime, Booking
 import datetime
+import locale
 
 main = Blueprint('main', __name__)
 
@@ -114,6 +115,26 @@ def result():
             seattypeID = request.form.get('seattype')
             
             return redirect(url_for('main.result', fromCityID=fromCityID, toCityID=toCityID, departing=departing, seattypeID=seattypeID))
+        
+        if action == 'book':
+            if not current_user.is_authenticated:
+                flash('Need login to book flight !', 'warning')
+            userId = current_user.user_id
+            flightId = request.form.get('flight_id')
+            booking_status = 'unpaid'
+            seattypeID = request.form.get('seat_type_id')
+            payment_amount = request.form.get('payment_amount')
+            booking_date = datetime.datetime.now()
+            
+            book = Booking(user_id=userId, flight_id=flightId, booking_status = booking_status, seat_type_id = seattypeID, payment_amount = payment_amount, booking_date=booking_date)
+
+            db.session.add(book)
+            db.session.commit()
+
+            # Redirect to the payment page with the newly created booking_id
+            flash('Booking successful! Proceed to payment.', 'success')
+            return redirect(url_for('main.pay', booking_id=book.booking_id))
+            
     fromCityID = request.args.get('fromCityID')
     toCityID = request.args.get('toCityID')
     departing = request.args.get('departing')
@@ -123,13 +144,9 @@ def result():
     seattypes = SeatType.query.all()
     cities = City.query.all()
     # return render_template('index.html', cities=cities, seattypes=seattypes)
-        
-    current_time = datetime.datetime.utcnow()
-    print(current_time)
-    return render_template('search_result.html', cities=cities, seattypes=seattypes, current_time=current_time)    
     
     query = text('''
-        SELECT f.flight_id, a.airline_name, c1.city_name, DATE_FORMAT(TIME(f.flight_date), '%H:%i') AS 'Departure Time', CONCAT(FLOOR(r.duration_hours/60), 'h ', (r.duration_hours%60), 'm') AS 'Duration', c2.city_name, DATE_FORMAT(DATE_ADD(TIME(f.flight_date), INTERVAL r.duration_hours MINUTE), '%H:%i') AS 'Arrival Time', FORMAT(f.flight_price + (f.flight_price * st.seat_type_price / 100), 2) AS 'price'
+        SELECT f.flight_id, a.airline_name, c1.city_name, DATE_FORMAT(TIME(f.flight_date), '%H:%i') AS 'Departure Time', CONCAT(FLOOR(r.duration_hours/60), 'h ', (r.duration_hours%60), 'm') AS 'Duration', c2.city_name, DATE_FORMAT(DATE_ADD(TIME(f.flight_date), INTERVAL r.duration_hours MINUTE), '%H:%i') AS 'Arrival Time', f.flight_price + (f.flight_price * st.seat_type_price / 100) AS 'price'
         FROM flights f
         JOIN routes r ON r.route_id = f.route_id
         JOIN airlines a ON a.airline_id = f.airline_id
@@ -152,6 +169,9 @@ def result():
     # flights = [result for result in results]
     flights = []
     for result in results:
+        total = result[7]
+        formatted_price = str(locale.format_string("%.0f", total, grouping=True))
+        formatted_price = formatted_price.replace(',', '.')
         flight = {
             'flight_id': result[0],
             'airline_name': result[1],
@@ -160,7 +180,9 @@ def result():
             'Duration': result[4],
             'Destination City': result[5],
             'Arrival Time': result[6],
-            'price' : result[7]
+            'payment_amount' : result[7],
+            'price' : formatted_price,
+            'seat_type_id' : seattypeID
         }
         flights.append(flight)
     print(flights)
